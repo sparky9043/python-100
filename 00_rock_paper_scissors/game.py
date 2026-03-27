0b2dd8d41b933d869d64df1cfa17a2d73f4732cc
Se Joon Park <sparky9043@gmail.com>
2026-03-27 23:08:58 +0900

    docs: add docstrings to Computer and Player methods

--- 00_rock_paper_scissors/game.py
+++ 00_rock_paper_scissors/game.py
@@ -1,3 +1,5 @@
+import random
+
 class Player:
     """Represents a player in the game
     
@@ -24,11 +26,30 @@ class Player:
         """Resets the score back to 0"""
         self.score = 0
     
-    def get_choice(self, index):
+    def get_choice(self, index) -> str:
+        """Take index as an argument and return rock, paper, or scissors"""
         return self.choices[index]
         
     def __str__(self):
+        """Returns string representation of Player objects"""
         return f"Player Name: {self.name} | Score: {self.score}"
+
+class Computer(Player):
+    """Represents computer player object
+
+    Attributes:
+        Player (Player): Inherit properties from the Player class
+    """
+    def __init__(self):
+        """Initializes Computer object by creating a Player Object
+        with the name computer
+        """
+        super().__init__("computer")
+        
+    def get_choice(self) -> str:
+        """Return a random choice from rock, paper, or scissors"""
+        index = random.randint(0, 2)
+        return super().get_choice(index)
         
 class Game:
     """Represents a game for the user against the computer
@@ -51,8 +72,8 @@ class Game:
             player2.name: player2.score,
         }
         
-player1 = Player('John')
-player2 = Player('computer')
+player1 = Player('john')
+player2 = Computer()
 
-game = Game(player1, player2)
-print(game)
+# game = Game(player1, player2)
+# print(game)
